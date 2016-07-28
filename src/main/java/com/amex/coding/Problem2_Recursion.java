52d3d7aa87225d67409aeea6e2b7de2c957a1f89
Thangaprabhu Chandrasekhar <TChandrasekhar@walmartlabs.com>
2016-07-27 17:23:54 -0700

    Updating

--- src/main/java/com/amex/coding/Problem2_Recursion.java
+++ src/main/java/com/amex/coding/Problem2_Recursion.java
@@ -4,12 +4,12 @@ public class Problem2_Recursion {
 
 	// Requirements: reverse the input string by using recursion.
 	public String reverseString(String inputStr) {
-		if(1 == inputStr.length()) {
+		if (1 == inputStr.length()) {
 			return inputStr;
 		}
-		return reverseString(inputStr.substring(1, inputStr.length()))+inputStr.charAt(0);
+		return reverseString(inputStr.substring(1, inputStr.length())) + inputStr.charAt(0);
 	}
-	
+
 	public static void main(String[] args) {
 		Problem2_Recursion reverseString = new Problem2_Recursion();
 		System.out.println(reverseString.reverseString("helloTest"));
